25b9b1f11ccb78fae0e7a559b07f9a98737f3bf2
ysm <ysm@ysm.com>
2025-10-30 11:35:13 +0800

    chores: update networkCoreSdk version

--- network-core-sdk-example/index.js
+++ network-core-sdk-example/index.js
@@ -2,20 +2,20 @@ const { PrimusNetwork } = require("@primuslabs/network-core-sdk");
 const { ethers } = require("ethers");
 require("dotenv/config");
 
-
 async function primusProofTest() {
   const PRIVATEKEY = process.env.PRIVATE_KEY;
   const address = "0x8F0D4188307496926d785fB00E08Ed772f3be890";
   const chainId = 84532; // Base Sepolia (or 8453 for Base mainnet)
   const baseSepoliaRpcUrl = "https://sepolia.base.org"; // (or 'https://mainnet.base.org' for Base mainnet)
   // The request and response can be customized as needed.
+  
   const requests = [
     {
       url: "https://www.okx.com/api/v5/public/instruments?instType=SPOT&instId=BTC-USD",
       method: "GET",
       header: {},
       body: "",
-    },
+    }
   ];
   const responseResolves = [
     [
@@ -27,6 +27,28 @@ async function primusProofTest() {
       },
     ],
   ];
+  // const responseResolves = [
+  //   [
+  //     {
+  //       keyName: "instIdCode",
+  //       parseType: "json",
+  //       parsePath: "$.data[0].instIdCode",
+  //       op: "<",
+  //       value: 200000,
+  //     },
+  //   ],
+  // ];
+  // const responseResolves = [
+  //   [
+  //     {
+  //       keyName: "instType",
+  //       parseType: "json",
+  //       parsePath: "$.data[0].instType",
+  //       op: "STREQ",
+  //       value: "SPOT",
+  //     },
+  //   ],
+  // ];
 
   const provider = new ethers.providers.JsonRpcProvider(baseSepoliaRpcUrl);
   const wallet = new ethers.Wallet(PRIVATEKEY, provider);
@@ -53,6 +75,7 @@ async function primusProofTest() {
       // attMode: {
       //   algorithmType: "proxytls", // optional, default is 'proxytls'
       // },
+      // getAllJsonResponse: "true", // optional, default is 'false',
     };
     let attestResult = await primusNetwork.attest(attestParams);
     console.log("Attest result:", attestResult);
@@ -66,6 +89,11 @@ async function primusProofTest() {
       verifyParams
     );
     console.log("Task result:", taskResult);
+
+    // const allJsonResponse = await primusNetwork.getAllJsonResponse(
+    //   attestResult[0].taskId
+    // );
+    // console.log("allJsonResponse:", allJsonResponse);
   } catch (error) {
     console.error("Unexpected error:", error);
     throw error;
