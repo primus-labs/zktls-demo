84176192093e09f5a06f37d463273926e25e63eb
ysm <ysm@ysm.com>
2026-01-28 16:12:00 +0800

    chores: update zktls-js-sdk version

--- test-example/README.md
+++ test-example/README.md
@@ -7,3 +7,18 @@ The main code file is this: https://github.com/primus-labs/zktls-demo/blob/main/
 npm install
 npm run dev
 ```
+
+## Configuration
+
+Before starting the attestation, you need to configure the following parameters in `src/testprimus.js`:
+
+### App ID and App Secret
+The **App ID** and **App Secret** in `src/testprimus.js` (lines 5-6) can be obtained from:
+- [Primus Developer Hub - My Projects](https://dev.primuslabs.xyz/myDevelopment/myProjects)
+- Create a new project or use an existing one to get your App ID and App Secret
+
+### Attestation Template ID
+The **Attestation Template ID** (`attTemplateID`) in `src/testprimus.js` (line 21) can be obtained from [Primus DevHub](https://dev.primuslabs.xyz/marketplace):
+- Browse and select a template from the marketplace
+- Or create your own custom template in the developer dashboard
+
--- test-example/src/testprimus.js
+++ test-example/src/testprimus.js
@@ -2,8 +2,10 @@ import { PrimusZKTLS } from "@primuslabs/zktls-js-sdk"
 
 // Initialize parameters.
 const primusZKTLS = new PrimusZKTLS();
-const appId = "0x17ae11d76b72792478d7b7bcdc76da9574ab3cf8";
-const appSecret= "0xafa01caf44f07d2b21bc5e2bde1de2a8ba56f33ac2e223169f99634f57d049b5";
+const appId = "YOUR_APPID";
+const appSecret= "YOUR_APPSECRET";
+const userAddress = "YOUR_USER_ADDRESS"; // 0x...
+const attTemplateID = "2e3160ae-8b1e-45e3-8c59-426366278b9d";
 //const initAttestaionResult = await primusZKTLS.init(appId, appSecret);
 
 // If it is running on a mobile terminal, you need to pass the platform parameter. The default platform is PC. If you add the following configuration, it can run on both PC and mobile terminals.
@@ -17,9 +19,6 @@ const initAttestaionResult = await primusZKTLS.init(appId, appSecret, {platform:
 console.log("primusProof initAttestaionResult=", initAttestaionResult);
 
 export async function primusProofTest() {
-    // Set TemplateID and user address.
-    const attTemplateID = "2e3160ae-8b1e-45e3-8c59-426366278b9d";
-    const userAddress = "0x7ab44DE0156925fe0c24482a2cDe48C465e47573";
     // Generate attestation request.
     const request = primusZKTLS.generateRequestParams(attTemplateID, userAddress);
 
